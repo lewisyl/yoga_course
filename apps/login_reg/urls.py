7e697f8658561e8fbc6c43e0f879c19b118cf4cc
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2019-09-26 12:33:47 -0700

    xiao change

--- apps/login_reg/urls.py
+++ apps/login_reg/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path ('', views.login, name='login'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path ('main_page/', views.main_page, name='main_page'),
+    path ('sign_in_user/', views.sign_in_user, name='sign_in_user'),
+    path ('sign_out/', views.sign_out, name='sign_out'),
 ]
--- apps/login_reg/views.py
+++ apps/login_reg/views.py
@@ -5,3 +5,20 @@ def login(req):
 
 def register(req):
     return HttpResponse("haha")
+
+def main_page(request):
+    print('*'*80)
+    if 'user' not in request.session:    
+        if request.method == "POST":
+            request.session['user']=request.POST['login_email']
+    return render(request, "home/index.html")
+
+def sign_in_user(request):
+    #save user id/name to session
+    print(request.POST.getlist('google')[0])
+    request.session['user'] = request.POST.getlist('google')[0]
+    return redirect('/login/main_page/')
+
+def sign_out(request):
+    del request.session['user']
+    return redirect('/home/')
